7a0f693717813040316a9721dd30d4cce3333ef9
Amin <34860@ma-web.nl>
2023-02-22 21:47:13 +0100

    typed language

--- 2022csharp1/2022csharp1/Program.cs
+++ 2022csharp1/2022csharp1/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
+            int leeftijd = 9;
+            bool finished = true;
+
+            float x = 0 if;
+            string gameName = "title";
+
             Console.WriteLine("Hello, World!");
         }
+
+
+    }
+
+    internal class GameEngine
+    {
+        GameEngineState state;
+        internal bool Load(string file)
+        {
+            return true;
+        }
+        internal void plus { GameEngine x, int y};
+    }
+    internal enum GameEngine
+    {
+    }
+    internal interface IGameEngine
+    {
     }
 }
