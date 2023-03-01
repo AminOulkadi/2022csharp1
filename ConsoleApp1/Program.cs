e9e2d4abbd0235701ded8243b74e49b926c53e4f
Amin <34860@ma-web.nl>
2023-03-01 22:54:55 +0100

    variables en rekenen opdracht

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //class variable
+        string programTitle = "variable demo";
+        int programWindowsPosx = 100;
+        static void Main(string[] args)
+        {
+            //type naam
+
+
+            //local variables
+            int xPos = 100;
+
+            xPos++;
+            xPos--;
+            xPos = xPos + 10 + 100 + 29;
+
+            xPos = (10 + 9) * 8;
+
+            Console.WriteLine(xPos);
+        }
+    }
+}
